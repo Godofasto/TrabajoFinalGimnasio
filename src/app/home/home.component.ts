a6f3edea71167ff3129eae61b916f082e20e3616
Godofasto <hiddennymeria@gmail.com>
2023-04-20 12:56:29 +0200

    1

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  logoUrl = 'assets/LogoB.png';
+  
+
+  constructor(
+    // private translate: TranslateService
+  ) {
+
+  }
+}
