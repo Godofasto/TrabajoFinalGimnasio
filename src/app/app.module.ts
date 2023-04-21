69a9eff1586184efae4eb3aa48d040aa0a9960bf
Godofasto <hiddennymeria@gmail.com>
2023-04-21 12:55:11 +0200

    21/04/2023 12:55

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent} from './body/body.component';
+import { CuotasComponent } from './cuotas/cuotas.component';
+import { RouterModule, Routes} from '@angular/router';
+
+const appRoutes:Routes=[
+
+  {path:'home', component: BodyComponent},
+  {path:'cuotas', component:CuotasComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    CuotasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
