6a35af0007fc8c77b124c71689f1809662050f4c
Godofasto <hiddennymeria@gmail.com>
2023-05-04 14:57:18 +0200

    15:00 04/05

--- src/app/catalogo/catalogo.component.ts
+++ src/app/catalogo/catalogo.component.ts
@@ -6,20 +6,20 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './catalogo.component.html',
   styleUrls: ['./catalogo.component.css']
 })
-export class CatalogoComponent implements OnInit {
+export class CatalogoComponent { //implements OnInit 
   cosas:any;
   prueba:any;
   modalOpen = false;
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:7104/api/productos/listado').subscribe((response)=>{
-      console.log(response);
-      this.cosas = response;
-    });
+  // ngOnInit(): void {
+  //   this.http.get('https://localhost:7104/api/productos/listado').subscribe((response)=>{
+  //     console.log(response);
+  //     this.cosas = response;
+  //   });
     
-    //Ver como se supone que tengo que quitar las comillas -> Se quitan quitando el |JSON
-  }
+  //   //Ver como se supone que tengo que quitar las comillas -> Se quitan quitando el |JSON
+  // }
   openModal(){
     if(this.modalOpen == false){
       this.modalOpen = true;
@@ -27,5 +27,29 @@ export class CatalogoComponent implements OnInit {
       this.modalOpen = false
     }
       
+  }
+  mostrarSuplementacion(){
+    this.http.get('https://localhost:7104/api/productos/listadoSuplementacion').subscribe((response)=>{
+      console.log(response);
+      this.cosas = response;
+    });
+  }
+  mostrarModa(){
+    this.http.get('https://localhost:7104/api/productos/listadoModa').subscribe((response)=>{
+      console.log(response);
+      this.cosas = response;
+    });
+  }
+  mostrarEquipamiento(){
+    this.http.get('https://localhost:7104/api/productos/listadoEquipamiento').subscribe((response)=>{
+      console.log(response);
+      this.cosas = response;
+    });
+  }
+  mostrarTodo(){
+    this.http.get('https://localhost:7104/api/productos/listado').subscribe((response)=>{
+      console.log(response);
+      this.cosas = response;
+    });
   }
 }
