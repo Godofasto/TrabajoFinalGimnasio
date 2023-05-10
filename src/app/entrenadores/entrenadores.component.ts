5114378923ec728af1135fff08fc290b842a4d05
Godofasto <hiddennymeria@gmail.com>
2023-05-10 11:09:48 +0200

    11:11 10/05/2023

--- src/app/entrenadores/entrenadores.component.ts
+++ src/app/entrenadores/entrenadores.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { SubirEntrenadorService } from '../subir-entrenador.service';
 
 @Component({
@@ -26,5 +27,27 @@ export class EntrenadoresComponent implements OnInit{
     }, (error) => {
       console.error(error);
     })
+  }
+  borrarProducto(Id : Number){ //Podria intentar ocultarlos en vez de eliminarlos directamente
+    console.log(this.cosas)
+    Swal.fire({
+      title: '¿Estas seguro?',
+      text: "No podrás recuperar este entrenador",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.http.delete(`https://localhost:7104/api/entrenadores/${Id}`).subscribe(()=>{
+        const indice = this.cosas.findIndex(p => p.id === Id);
+        this.cosas.splice(indice, 1); //Esto es para que se vea en local el cambio porque recargar la pagina es cutre
+    })
+        
+      }
+    })
+    
   }
 }
--- src/app/subir-entrenador.service.ts
+++ src/app/subir-entrenador.service.ts
@@ -9,12 +9,12 @@ export class SubirEntrenadorService {
   constructor(private http:HttpClient) { }
 
   subirEntrenador(datos : any){
-    return this.http.post('https://localhost:7104/api/entrenadores/RetornarTodosEntrenadores', datos);
+    return this.http.post('https://localhost:7104/api/entrenadores/SubirEntrenadores', datos);
   }
   recogerEntrenadores(dato: any){
     // var req ={ //Muy importante que el atributo se llame igual aqui que en el .NET, antes no eran TipoDato los dos y fallaba
     //   TipoDato : dato
     // }
-    return this.http.post('https://localhost:7104/api/entrenadores/SubirEntrenadores', {});
+    return this.http.post('https://localhost:7104/api/entrenadores/RetornarTodosEntrenadores', {});
   }
 }
